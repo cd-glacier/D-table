5bddacd6d4c4f9586d5242fb175616bff078e89c
hyoga <hyoga0216@gmail.com>
2015-09-04 17:58:05 +0900

    make week in D-table

--- app.rb
+++ app.rb
@@ -13,6 +13,15 @@ end
 
 
 get "/D_table" do
-	erb :D_table
+	@day_hash = {0 => "Mon", 1 => "Tue", 2 => "Wed", 3 => "Thu", 4 => "Fri", 5 => "Sat"}
+	erb :D_table, :local => { :style => "D_table" }
 end
 
+get "/login" do
+	erb :login
+end
+
+get "/subjects/:code" do
+	params['code']
+	erb :subjects
+end
