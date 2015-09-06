290d111625a8b8ec3cc4d1367f87d43044c8adf3
hyoga <hyoga0216@gmail.com>
2015-09-07 02:42:38 +0900

    デザイン変えるので一応

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'active_record'
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection('development')
 
+set :public_folder, File.dirname(__FILE__) + '/public'
+
 =begin
 class Memos < ActiveRecord::Base
 end
@@ -13,8 +15,9 @@ end
 
 
 get "/D_table" do
+	@style = "D_table"
 	@day_hash = {0 => "Mon", 1 => "Tue", 2 => "Wed", 3 => "Thu", 4 => "Fri", 5 => "Sat"}
-	erb :D_table, :local => { :style => "D_table" }
+	erb :D_table
 end
 
 get "/login" do
